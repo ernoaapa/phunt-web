0c67245626c91390e2d5656dd8dde17957d29752
Kalle <carlpetter.bertell@gmail.com>
2012-03-10 01:31:29 +0200

    removed json wrapper

--- app/controllers/api/v1/Chains.java
+++ app/controllers/api/v1/Chains.java
@@ -2,7 +2,6 @@ package controllers.api.v1;
 
 import javax.inject.Inject;
 
-import models.JsonResponse;
 import services.ChainService;
 
 public class Chains extends AuthenticatedController {
@@ -11,7 +10,7 @@ public class Chains extends AuthenticatedController {
 	private static ChainService chainService;
 
 	public static void heads(String uuid) {
-		JsonResponse response = new JsonResponse(chainService.findHeads());
-		renderJSON(response);
+
+		renderJSON(chainService.findHeads());
 	}
 }
