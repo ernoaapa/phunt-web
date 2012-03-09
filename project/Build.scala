814240986c80ac62d90b80119940996b83d1ab23
Erno Aapa <erno.aapa@finflex.fi>
2012-03-09 08:44:13 +0200

    renamed project to phunt

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "Phunt-web"
+    val appName         = "phunt"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
