f73b97ace74a36c879207add090c5ad701172d4a
Erno Aapa <erno.aapa@finflex.fi>
2012-03-10 08:35:36 +0200

    created verify location api and test ui

---
+++ app/controllers/Location.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Location extends Controller {
+	
+	public static void verify() {
+        render();
+    }
+}
--- app/controllers/api/v1/Locations.java
+++ app/controllers/api/v1/Locations.java
@@ -1,12 +1,23 @@
 package controllers.api.v1;
 
-import javax.inject.Inject;
+import models.Location;
 
-import services.LocationService;
+import com.javadocmd.simplelatlng.LatLngTool;
+import com.javadocmd.simplelatlng.util.LengthUnit;
 
 public class Locations extends AuthenticatedController {
 
-	@Inject
-	private static LocationService locationService;
-
+	public void verify(Long locationId) {
+		Location location = Location.findById(locationId);
+		
+		if (location == null) {
+			error(500, "Invalid location id!");
+		}
+		
+		if(LatLngTool.distance(getRequestLatLng(), location.asLatLng(), LengthUnit.METER) < 20) {
+			render("OK");
+		} else {
+			render("ERROR");
+		}
+	}
 }
