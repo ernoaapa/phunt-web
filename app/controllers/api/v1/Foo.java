1559de36ea4125eee3cfd9352623457007c4c4e0
Kalle <carlpetter.bertell@gmail.com>
2012-04-15 23:09:23 +0300

    Foo for testing

---
+++ app/controllers/api/v1/Foo.java
@@ -0,0 +1,38 @@
+package controllers.api.v1;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import models.Location;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import play.Logger;
+import play.db.DB;
+
+public class Foo extends AuthenticatedController {
+
+	public static void show() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(DB.datasource);
+		final AtomicInteger cnt = new AtomicInteger();
+		
+		Logger.info("Datasource: "+DB.datasource);
+		List locations = jdbcTemplate.query("SELECT *, earth_distance(ll_to_earth(lat, lon), ll_to_earth(60.233312, 25.1)) AS distance FROM location", new RowMapper() {
+			@Override public Object mapRow(ResultSet resultSet, int arg1) throws SQLException {
+				Logger.info("Fetching "+cnt.incrementAndGet());
+				
+				Location loc = new Location();
+				loc.roughDistance = resultSet.getDouble("distance")+"";
+				loc.id = resultSet.getLong("id");
+				return loc;
+			}
+			
+		});
+				
+		renderJSON(locations);
+	}
+	
+}
